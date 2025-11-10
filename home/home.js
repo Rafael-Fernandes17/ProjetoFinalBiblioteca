1e8bc4badb103ff5f1b0eba4ff837d995b562d7d
Rafael-Fernandes17 <rafael.botelho.fernandes16@gmail.com>
2025-11-10 20:49:10 -0300

    Primeiro Commit Pagina Home
    Principal estrutural html, css e js

---
+++ home/home.js
@@ -0,0 +1,10 @@
+//Configurações da API
+const API_URL = 'https://jsonplaceholder.typicode.com/users';
+
+// Capturando variaveis
+const form = document.getElementById("meuFormulario");
+const mensagem = document.getElementById("mensagens");
+const readBtn = document.getElementById('readBtn');
+const updateBtn = document.getElementById('updateBtn');
+const deleteBtn = document.getElementById('deleteBtn');
+
